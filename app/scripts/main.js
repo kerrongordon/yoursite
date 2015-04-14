f1e8cae14ef36b6c253036532aa505185500f728
kerrongordon <kgpsounds.com@gmail.com>
2015-04-14 19:53:04 -0400

    added side menu to the site need some more fixing to be done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,87 +1,174 @@
 'use strict';
 
-
-console.log('Hodd luck!');
-
 function yoursite() {
-	
-	//header 
-	$(window).scroll(function() {
-		var wScroll  = $(this).scrollTop(),
-		    skillBar = $('.skill-bar'),
-		    logo     = $('.logo'),
-		    header   = $('header').height()/1.8,
-		    overlay  = $('.overlay');
 
-		//logo.css({ 'margin-top' : wScroll });
-		logo.css('transform', 'translate(0,' + wScroll/2 + 'px )');
+	var kgProj           = $('.proj'),
+		kgPageLoad 	     = $('.page-load'),
+		kgProjects       = $('.projects'),
+		kgProjSlide      = $('.proj-slide'),
+		kgClose          = $('.close'),
+		kgWaterdrop      = $('.skill-bar'),
+		kgSkill          = $('.skill'),
+		kgSkillBar       = $('.skill-bar'),
+		kgLogo           = $('.logo'),
+		kgHeader         = $('header'),
+		kgOverlay        = $('.overlay'),
+		kgMenubar        = $('.menubar'),
+		kgMenubarLink    = $('.menubar a'),
+		kgProjectsLink   = $('#projects'),
+		kgPage           = $('.page'),
+		kgMenubutton     = $('.menubutton');
 
-		overlay.css('opacity', wScroll /600 );
 
-		if (wScroll <= header) {
-	        skillBar.addClass('skill-level-0 fadeInWaterdrop');
-	    } else {
-	        skillBar.removeClass('skill-level-0 fadeInWaterdrop');
-	    }
+	//scroll page logo
 
-	});
+	function logoScroll() {
 
-		var waterdrop = $('.skill-bar'),
-			skill     = $('.skill');
+		$(window).scroll(function() {
+			var wScroll  = $(this).scrollTop(),
+			    header   = kgHeader.height()/1.8;
 
-		waterdrop.each(function() {
-			
-			var waterSpan 	= $(this).find('span'),
-			 	skillWidth 	= skill.width(),
-			 	barWidth  	= $(this).width(),
-			 	total 		= Math.floor((barWidth / skillWidth) * 100);
+			kgLogo.css('transform', 'translate(0,' + wScroll/2 + 'px )');
 
-			waterSpan.text(total + '');
+			kgOverlay.css('opacity', wScroll /600 );
+
+			if (wScroll <= header) {
+		        kgSkillBar.addClass('skill-level-0');
+		    } else {
+		        kgSkillBar.removeClass('skill-level-0');
+		    }
 
 		});
 
-		var proj        = $('.proj'),
-			pageLoad 	= $('.page-load'),
-			projects    = $('.projects'),
-			projSlide   = $('.proj-slide'),
-			close       = $('.close');
+	}
 
-	pageLoad.load('projects/loading.html');
 
-	proj.on('click', function() {
-		var pageId      = $(this).attr('id');
+
+	//Gets the skill level form document
+
+	function waterdropCount() {
+
+		kgWaterdrop.each(function() {
+				
+			var waterSpan 	= $(this).find('span'),
+			 	skillWidth 	= kgSkill.width(),
+			 	barWidth  	= $(this).width(),
+			 	total 		= Math.floor((barWidth / skillWidth) * 100);
+
+				waterSpan.text(total + '');
+
+		});
+
+	}
+
+
+	//Projects init load Page
+	kgPageLoad.load('projects/loading.html');
+
+
+	//Projects Open Proj
+
+	function projectsOpen() {
+
+		kgProj.on('click', function() {
+			var pageId      = $(this).attr('id');
+			
+			kgPageLoad.load('projects/' + pageId + '.html');
+
+			console.log();
+
+			kgProjSlide.css('left', '-50%');
+
+			kgProjects.delay(500)
+				.queue( function(next){ 
+	            	$(this).animate({height: '200px'}, 30);
+	            	$('html,body').animate({scrollTop: kgProjectsLink.offset().top}, 1000);
+	            next();
+	        });
+
+		});
+
+	}
+
+
+	//Projects Close button
+
+	function projectsClose() {
+
+		kgClose.click(function() {
+
+			kgProjSlide.css('left', '0%');
+			
+			kgProjects.css('height', 'auto')
+			.delay(900)
+				.queue( function(next){ 
+	            	kgPageLoad.load('projects/loading.html');
+	            next();
+	        });
+
+		});
+
+	}
+
+
+
+	//page scroll to id 
+
+	function pageScroll() {
+
+			$('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		     	}
+		    }
+		});
+
+	}
+
+
+	// open and close menu
+
+	function mainmenubutton() {
+
+		kgMenubutton.on('click', function() {
+			kgMenubar.toggleClass('menu-open');
+		});
+
+		kgMenubarLink.on('click', function() {
+			kgMenubar.removeClass('menu-open');
+		});
 		
-		pageLoad.load('projects/' + pageId + '.html');
+		kgPage.on('click', function() {
+			kgMenubar.removeClass('menu-open');
+		});
 
-		console.log();
+	}
+    
+    	//call all functions 
 
-		projSlide.css('left', '-50%');
+		pageScroll();
 
-		projects.delay(500)
-			.queue( function(next){ 
-            	$(this).animate({height: '200px'}, 10);
-            	location.href = '#projects'; 
-            next();
-        });
+		logoScroll();
 
-	});
+		waterdropCount();
 
+		projectsOpen();
 
+		projectsClose();
 
-	close.click(function() {
-
-		projSlide.css('left', '0%');
-		
-		projects.css('height', 'auto')
-		.delay(900)
-			.queue( function(next){ 
-            	pageLoad.load('projects/loading.html');
-            next();
-        });
-	
-	});
-
+		mainmenubutton();
 
 }
 
-yoursite();
+
+$(function() {
+
+	yoursite();
+
+});
